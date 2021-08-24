ab5e370e0d612a04379181d2b0861ffde3f4c513
zchengyun <1690132420@qq.com>
2021-08-25 01:46:25 +0800

    添加项目文件。

---
+++ bad_weak_ptr.cpp
@@ -0,0 +1,17 @@
+﻿//std::bad_weak_ptr是当std::weak_ptr引用已删除的对象时，以std::weak_ptr作为参数的std::shared_ptr构造函数作为异常抛出的对象类型。
+#include <iostream>
+#include <memory>
+int main()
+{
+	std::shared_ptr<int> p1(new int(10));
+	std::weak_ptr<int> p2(p1); //std::weak_ptr引用已删除的对象
+	p1.reset();
+	try
+	{
+		std::shared_ptr<int> p3(p2);//std::weak_ptr作为参数的std::shared_ptr构造函数
+	}
+	catch (const std::bad_weak_ptr& e)
+	{
+		std::cout << e.what();
+	}
+}
